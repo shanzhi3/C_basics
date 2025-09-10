fc7d5aa55ddacd79917d8c397649493d93874aae
shanzhi3 <2029855521@qq.com>
2025-09-11 00:24:20 +0800

    矩阵计算.cpp

--- 矩阵计算.cpp
+++ 矩阵计算.cpp
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
-void matrix_multiply(int A[3][3], int B[3][3], int result[3][3]) {
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
+void matrix_multiply(int A[3][3], int B[3][3], int result[3][3]){
+    for (int i = 0; i < 3; i++){
+        for (int j = 0; j < 3; j++){
             result[i][j] = 0;
-            for (int k = 0; k < 3; k++) {
+            for (int k = 0; k < 3; k++){
                 result[i][j] += A[i][k] * B[k][j];
             }
         }
     }
 }
 
-int main() {
+int main(){
     int A[3][3] = {{1, 2, 3},
                    {4, 5, 6},
                    {7, 8, 9}};
@@ -23,8 +23,8 @@ int main() {
     matrix_multiply(A, B, result);
 
     printf("Result:\n");
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
+    for (int i = 0; i < 3; i++){
+        for (int j = 0; j < 3; j++){
             printf("%d ", result[i][j]);
         }
         printf("\n");
@@ -32,3 +32,4 @@ int main() {
 
     return 0;
 }
+
