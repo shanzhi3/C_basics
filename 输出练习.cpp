faab4d0ce4a6341f825507981351d2c8135a3e00
shanzhi3 <2029855521@qq.com>
2025-09-12 22:18:05 +0800

    Add files via upload
    今天做的不多，非常简单

---
+++ 输出练习.cpp
@@ -0,0 +1,74 @@
+/* 
+#include<stdio.h>
+
+int jolly();
+int deny();
+
+int jolly(){
+	int i;
+	for(i = 0; i < 3 ;i++){
+		printf("For he's a jolly good fellow!\n");
+	}
+	return 0;
+} 
+
+int deny(){
+	printf("Which nobody can deny");
+	return 0;
+}
+
+int main(){
+	jolly();
+	deny();
+	return 0;
+} 
+*/
+
+
+/*
+#include<stdio.h>
+void output_smile();
+
+void output_smile(){
+	printf("smile!");
+}
+
+int main(){
+	int i;
+	for(i = 0; i < 3; i++){
+		output_smile();	
+	}
+	printf("\n");
+	
+	for(i = 0; i < 2; i++){
+		output_smile();	
+	}
+	printf("\n");
+	
+	output_smile();
+	return 0;
+}
+*/
+ 
+/*
+ #include<stdio.h>
+ void two();
+ void one_three();
+ 
+ void two(){
+ 	printf("two\n");
+ }
+ void one_three(){
+ 	printf("one\n");
+ 	two();
+ 	printf("three\n");	
+ }
+ 
+ int main(){
+ 	one_three();
+ 	printf("done!\n");
+ 	return 0;
+ }
+*/
+ 
+
